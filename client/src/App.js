9d8799e5ef842f1a874028cb2191be90174ce4e0
Estefan Hu <estefanhu074@gmail.com>
2020-05-10 03:00:43 -0700

    Installed boilers

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import Cookie from 'js-cookie';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css';
 
 export const App = () => {
+  const [lng, setLng] = useState(null);
+  const [lat, setLat] = useState(null);
+
+  useEffect(() => {
+    fetch('http://ip-api.com/json')
+      .then(res => res.json())
+      .then(console.log)
+      .catch(console.error);
+  })
+
   return (
     <div>
 
