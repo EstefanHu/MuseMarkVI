8904f6daf47abcdec28edeaa1a957b40b0062dc4
Estefan Hu <estefanhu074@gmail.com>
2020-05-10 02:27:58 -0700

    Corrected models assignment

--- server/models/user.js
+++ server/models/user.js
@@ -1,40 +1,72 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
-const storySchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    default: ''
-  },
-  description: {
-    type: String,
-    required: true,
-    default: ''
-  },
-  genre: {
-    type: String,
-    requried: true,
-    default: ''
-  },
-  credibility: {
-    type: Number,
-    default: 0
-  },
-  location: {
-    type: [Number],
-    default: 0
-  },
-  author: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        default: ''
+    },
+    lastName: {
+        type: String, 
+        required: true,
+        trim: true,
+        default: ''
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 5,
+        default: ''
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        default: ''
+    },
+    credibility: {
+      type: Number,
+      default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
 }, {
-  timestamps: true
+    timestamps: true
 });
 
-module.exports = mongoose.model('Story', storySchema);
+// https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
+userSchema.pre('save', function(next) {
+    let user = this;
+    if(!user.isModified('password')) return next();
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model('User', userSchema);
