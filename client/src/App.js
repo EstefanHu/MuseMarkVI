cda94069c79b0041eaa2214e3a615983e88ad4e5
Estefan Hu <estefanhu074@gmail.com>
2020-05-10 03:10:22 -0700

    Set Authorized route

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,22 @@ import Cookie from 'js-cookie';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css';
 
+const checkAuth = () => {
+  const cookie = Cookie.get('museCookie');
+  if (!cookie) return false;
+  return true;
+}
+
+const AuthRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    checkAuth() ? (
+      <Component {...props} />
+    ) : (
+        <Redirect to={{ pathname: '/login' }} />
+      )
+  )} />
+)
+
 export const App = () => {
   const [lng, setLng] = useState(null);
   const [lat, setLat] = useState(null);
@@ -22,8 +38,12 @@ export const App = () => {
   })
 
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/(|register|login|privacy|terms|forgot)' component={Landing} />
+        <AuthRoute exact path='/app/(|dashboard|create|community|settings)' component={Primary} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </Router>
   )
 }
