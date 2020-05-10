bfbdd876a7c87b36da7c009fcb50a8f233f3d3a5
Estefan Hu <estefanhu074@gmail.com>
2020-05-10 02:26:29 -0700

    added models

---
+++ server/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const storySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    default: ''
+  },
+  description: {
+    type: String,
+    required: true,
+    default: ''
+  },
+  genre: {
+    type: String,
+    requried: true,
+    default: ''
+  },
+  credibility: {
+    type: Number,
+    default: 0
+  },
+  location: {
+    type: [Number],
+    default: 0
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Story', storySchema);
