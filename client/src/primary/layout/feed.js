2bf54fc40349fdf9956b9b1cf6bc68983a7b3446
Estefan Hu <estefanhu074@gmail.com>
2020-05-11 14:09:17 -0700

    installed components

--- client/src/primary/layout/feed.js
+++ client/src/primary/layout/feed.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import { Dashboard } from '../dashboard';
+import { Community } from '../community';
+import { New } from '../new';
 
 const feed = {
   display: 'flex',
@@ -19,7 +24,9 @@ const closed = {
 export const Feed = () => {
   return (
     <section style={feed}>
-
+      <Route exact path='/app/dashboard' component={Dashboard} />
+      <Route exact path='/app/new' component={New} />
+      <Route exact path='/app/community' component={Community} />
     </section>
   )
 }
