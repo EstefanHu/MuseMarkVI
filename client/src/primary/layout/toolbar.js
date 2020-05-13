f8aef2ffb3247421824ecf0c043d244b9c9a0027
Estefan Hu <estefanhu074@gmail.com>
2020-05-12 18:02:08 -0700

    converted toolbar to Router logic

--- client/src/primary/layout/toolbar.js
+++ client/src/primary/layout/toolbar.js
@@ -1,49 +1,26 @@
-import React, { useContext } from 'react';
-import { EntryContext } from '../../context';
+import React from 'react';
 import {
-  withRouter,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 import { FiPlusSquare } from 'react-icons/fi';
 
-export const Toolbar = withRouter(props => {
-  const { entry, setEntry } = useContext(EntryContext)
-
-  const createEntry = () => {
-    setEntry({});
-    props.history.push('/app/new');
-  }
-
-  const cancelEntry = () => {
-    setEntry(null);
-    props.history.push('/app/dashboard');
-  }
-
-  return (
-    <nav id='toolbar' className='topNav'>
-      <h1 className='logo'>:M</h1>
-      <span>
-        <Route
-          exact
-          path='/app/(dashboard|community|settings)'
-          component={
-            <FiPlusSquare
-              className='toolbar__icons'
-              onClick={createEntry}
-            />
-          }
-        />
-        <Route
-          exact
-          path='/app/create'
-          component={
-            <button
-              className='toolbar__button'
-              onClick={cancelEntry}
-            >Cancel</button>
-          }
-        />
-      </span>
-    </nav >
-  )
-});
+export const Toolbar = () => (
+  <nav id='toolbar' className='topNav'>
+    <h1 className='logo'>:M</h1>
+    <span>
+      <Route exact path='/app/(dashboard|community|settings)'>
+        <Link to='/app/new'>
+          <FiPlusSquare className='toolbar__icons' />
+        </Link>
+      </Route>
+      <Route exact path='/app/new'>
+        <Link to='/app/dashboard'>
+          <button
+            className='toolbar__button'
+          >Cancel</button>
+        </Link>
+      </Route>
+    </span>
+  </nav >
+);
