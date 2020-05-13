747ee1db517434458dfe3363dc123364c548730c
Estefan Hu <estefanhu074@gmail.com>
2020-05-13 15:49:12 -0700

    beginning debugging of lngLat rendering

--- client/src/primary/layout/mapfeatures/mapcommunity.js
+++ client/src/primary/layout/mapfeatures/mapcommunity.js
@@ -1,12 +1,25 @@
 import React, { useContext } from 'react';
 import { SubFeedContext } from '../../../context';
+import { Marker } from 'mapbox-gl';
+import { MdLocationOn } from 'react-icons/md';
+
+const icon = {
+  width: '40px',
+  height: '40px',
+}
 
 export const MapCommunity = () => {
   const { subFeed } = useContext(SubFeedContext);
 
-  return subFeed && (
-    <>
-      
-    </>
-  )
+  return subFeed.map(item => (
+    <Marker
+      latitude={item.coordinates[1]}
+      longitude={item.coordinates[0]}
+      offsetLeft={-25}
+      offsetTop={-47}
+    >
+      <MdLocationOn
+        style={icon} />
+    </Marker>
+  ))
 }
