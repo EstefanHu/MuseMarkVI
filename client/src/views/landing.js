dce8bcb65cad87d406f5ec6c3940abf31e161c25
Estefan Hu <estefanhu074@gmail.com>
2020-06-03 21:39:02 -0700

    added content

--- client/src/views/landing.js
+++ client/src/views/landing.js
@@ -2,71 +2,24 @@ import React from 'react';
 
 export const Landing = () => (
   <>
-    <section
-      className='parallax__layer parallax__back'
-    >
+    <section className='parallax__layer parallax__back'>
       <h1 id='hero__text'>
         Project<br />:Muse
     </h1>
-      <footer>
-
-      </footer>
     </section>
-    <div
-      style={landingcontent}
-      className='parallax__layer'
-    >
 
-      <section style={content}>
-        <h2 style={header}>Welcome to the Muse Project</h2>
-        <p style={p}>Project:Muse is an experimental story telling platform <br />
-          that accentuates the world around us.</p>
+    <div className='parallax__layer'>
+      <section className='landingContent'>
+        <div>
+          <h1>Welcome to Project:Muse</h1>
+          <p>Project:Muse is an experimental story telling platform that prioritizes the context of the story over its popularity.</p>
+          <p></p>
+        </div>
+        <div>
+          <h2>Why localization is important</h2>
+          <p></p>
+        </div>
       </section>
-
-      <section style={content}>
-        <h2 style={header}>Welcome to the Muse Project</h2>
-        <p style={p}>Project:Muse is an experimental story telling platform <br />
-          that accentuates the world around us.</p>
-      </section>
-
-      <section style={content}>
-        <h2 style={header}>Welcome to the Muse Project</h2>
-        <p style={p}>Project:Muse is an experimental story telling platform <br />
-          that accentuates the world around us.</p>
-      </section>
-
-      <section style={content}>
-        <h2 style={header}>Welcome to the Muse Project</h2>
-        <p style={p}>Project:Muse is an experimental story telling platform <br />
-          that accentuates the world around us.</p>
-      </section>
-
     </div>
   </>
 );
-
-const landingcontent = {
-  // transform: 'translateZ(0)',
-  paddingLeft: '10px'
-}
-
-const content = {
-  position: 'relative',
-  fontFamily: '"Roboto", sans-serif',
-  borderRadius: '20px',
-  boxShadow: '2px 2px 10px rgba(0,0,0,0.2)',
-  background: 'white',
-  width: 'fit-content',
-  padding: '30px 50px',
-  marginTop: '500px'
-}
-
-const header = {
-  fontSize: '2.3rem',
-  fontWeight: '700'
-}
-
-const p = {
-  fontSize: '1.4rem',
-  fontWeight: '500'
-}
